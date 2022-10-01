48014790b18d1d4b8567a8d6bb1e6a9836e21bba
jiajunwan <jiajunwan2015@gmail.com>
2022-10-01 01:04:21 +0000

    first commit

---
+++ AlexNet.py
@@ -0,0 +1,57 @@
+import torch.nn as nn
+import torchvision.models as models
+
+
+model_urls = {
+    'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
+}
+
+
+class LocalizerAlexNet(nn.Module):
+    def __init__(self, num_classes=20):
+        super(LocalizerAlexNet, self).__init__()
+        # TODO (Q1.1): Define model
+
+
+    def forward(self, x):
+        # TODO (Q1.1): Define forward pass
+
+        return x
+
+
+class LocalizerAlexNetRobust(nn.Module):
+    def __init__(self, num_classes=20):
+        super(LocalizerAlexNetRobust, self).__init__()
+        # TODO (Q1.7): Define model
+
+
+    def forward(self, x):
+        # TODO (Q1.7): Define forward pass
+
+        return x
+
+
+def localizer_alexnet(pretrained=False, **kwargs):
+    r"""AlexNet model architecture from the
+    `"One weird trick..." <https://arxiv.org/abs/1404.5997>`_ paper.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = LocalizerAlexNet(**kwargs)
+    # TODO (Q1.3): Initialize weights based on whether it is pretrained or not
+
+    return model
+
+
+def localizer_alexnet_robust(pretrained=False, **kwargs):
+    r"""AlexNet model architecture from the
+    `"One weird trick..." <https://arxiv.org/abs/1404.5997>`_ paper.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = LocalizerAlexNetRobust(**kwargs)
+    # TODO (Q1.7): Initialize weights based on whether it is pretrained or not
+
+    return model
